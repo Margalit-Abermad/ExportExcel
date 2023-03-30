957db1d7a11a02f575ab0969347233b57d2c7a06
Margalit-Abermad <2329767+0@gmail.com>
2023-03-30 16:40:00 +0300

    commit project

---
+++ ExportToExcel/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Ganss.Excel;
+using System.Collections.Generic;
+
+
+List<Student> students = new List<Student>()
+{
+    new Student{Name="Margalit", LastName="Abermad" },
+    new Student{Name="Sara", LastName ="Cohen"}
+};
+
+ExcelMapper mapper = new ExcelMapper();
+var File = @"C:\Users\user\Music\Marga.xlsx";
+mapper.Save(File, students,"File",true);
+
+Console.ReadKey();
+
+class Student
+{
+    public string Name { get; set; }
+
+    public string LastName { get; set; }
+}
